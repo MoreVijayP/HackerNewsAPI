2227dd4933389db832e2515655908d4df0464d32
MoreVijayP <160055785+MoreVijayP@users.noreply.github.com>
2024-02-14 22:12:25 +0400

    Add files via upload

--- HackerNewsAPI/WebApplication1/StartupLoadData.cs
+++ HackerNewsAPI/WebApplication1/StartupLoadData.cs
@@ -8,34 +8,42 @@ namespace HackerNewsAPI
         private static readonly HttpClient _client = new HttpClient();
         public static void InvokeAsync()
         {
-            string projectDirectory = Directory.GetCurrentDirectory();
-
-            DataRepository dataRepository = new DataRepository(projectDirectory + "/Repo/Data.json");
-            string apiUrl = "https://hacker-news.firebaseio.com/v0/beststories.json";
-
-            // Make GET request to the API
-            HttpResponseMessage response =  _client.GetAsync(apiUrl).GetAwaiter().GetResult();
-
-            if (response.IsSuccessStatusCode)
+            try
             {
-                string responseBody =  response.Content.ReadAsStringAsync().GetAwaiter().GetResult();
-                List<int> bestStories = JsonConvert.DeserializeObject<List<int>>(responseBody);
-                List<StoryItem> stories = new List<StoryItem>();
-                foreach (var item in bestStories)
-                {
-                    string apiDetailUrl = "https://hacker-news.firebaseio.com/v0/item/" + item + ".json";
+                string projectDirectory = Directory.GetCurrentDirectory();
 
-                    // Make GET request to the API
-                    HttpResponseMessage responseDetails =  _client.GetAsync(apiDetailUrl).GetAwaiter().GetResult();
-                    if (responseDetails.IsSuccessStatusCode)
+                DataRepository dataRepository = new DataRepository(projectDirectory + "/Repo/Data.json");
+                string apiUrl = "https://hacker-news.firebaseio.com/v0/beststories.json";
+
+                // Make GET request to the API
+                HttpResponseMessage response = _client.GetAsync(apiUrl).GetAwaiter().GetResult();
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string responseBody = response.Content.ReadAsStringAsync().GetAwaiter().GetResult();
+                    List<int> bestStories = JsonConvert.DeserializeObject<List<int>>(responseBody);
+                    List<StoryItem> stories = new List<StoryItem>();
+                    foreach (var item in bestStories)
                     {
-                        string rsBody =  responseDetails.Content.ReadAsStringAsync().GetAwaiter().GetResult();
-                        StoryItem storyItem = JsonConvert.DeserializeObject<StoryItem>(rsBody);
-                        stories.Add(storyItem);
+                        string apiDetailUrl = "https://hacker-news.firebaseio.com/v0/item/" + item + ".json";
+
+                        // Make GET request to the API
+                        HttpResponseMessage responseDetails = _client.GetAsync(apiDetailUrl).GetAwaiter().GetResult();
+                        if (responseDetails.IsSuccessStatusCode)
+                        {
+                            string rsBody = responseDetails.Content.ReadAsStringAsync().GetAwaiter().GetResult();
+                            StoryItem storyItem = JsonConvert.DeserializeObject<StoryItem>(rsBody);
+                            stories.Add(storyItem);
+                        }
                     }
+                    dataRepository.WriteData(stories);
                 }
-                dataRepository.WriteData(stories);
             }
+            catch (Exception ex)
+            {
+                //log exception
+            }
+           
 
         }
     }
